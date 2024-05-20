b614f255fb856cc230a0085b83413d66513f5679
username <useremail@qq.com>
2024-05-20 12:06:38 +0800

    21

--- cleanGit.js
+++ cleanGit.js
@@ -1,26 +1,39 @@
 const puppeteer = require('puppeteer');
-const {dealCoding} = require("./coding");
-const {dealGitCode} = require("./gitCode");
-const {dealMayun} = require("./mayun");
+const { dealCoding } = require("./coding");
+const { dealGitCode } = require("./gitCode");
+const { dealMayun } = require("./mayun");
 
 let lastDealDate = '';
 (async () => {
     const browser = await puppeteer.connect({ browserURL: 'http://localhost:9222' });
 
-    while(true){
-        console.log('start')
+    while (true) {
+        // console.log('start')
         const now = new Date();
         const year = now.getFullYear();
         const month = now.getMonth();
         const day = now.getDate();
-        if(lastDealDate != `${year}-${month}-${day}`){
+        const hour = now.getHours();
+        if (lastDealDate != `${year}-${month}-${day}-${hour}`) {
             console.log('dealdeal')
-            await dealCoding(browser);
-            await dealGitCode(browser);
-            await dealMayun(browser);
-            lastDealDate = `${year}-${month}-${day}`;
+            try {
+                await dealCoding(browser);
+            } catch (e) {
+                console.error(e)
+            }
+            try {
+                await dealGitCode(browser);
+            } catch (e) {
+                console.error(e)
+            }
+            try {
+                await dealMayun(browser);
+            } catch (e) {
+                console.error(e)
+            }
+            lastDealDate = `${year}-${month}-${day}-${hour}`;
         }
-        console.log('end')
+        // console.log('end')
         await new Promise(r => setTimeout(r, 5000));
     }
 })();
